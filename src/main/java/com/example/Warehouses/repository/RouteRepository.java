67a3bd3eb7a7806e2f6cb12235961df5c0bc6efb
kadam Tejeshwar sai <kadamtejesh07@gmail.com>
2026-09-06 10:47:24 +0530

    Delete src directory

--- src/main/java/com/example/Warehouses/repository/RouteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Warehouses.repository;
-
-import com.example.Warehouses.entity.Route;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RouteRepository extends JpaRepository<Route,Long>{
-
-}
